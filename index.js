b56bcb0cfda6889e0d0c99963796b6921ee48b35
Suraj Kumar <surajsinhaking77@gmail.com>
2023-03-20 23:29:38 +0530

    update some

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const http = require('http');
 const data = require('./data');
 
-http.createServer((request, response)=> {
-    response.writeHead(200, {'Content-type':'application\json'});
-    response.write(JSON.stringify(data));
+let app = http
+
+app.createServer((request, response)=> {
+    response.writeHead(200, {'Content-type':'application\json'});  // header infomation and 
+    response.write(JSON.stringify(data));                          // to convert json data in object
     response.end();
-}).listen(7000);
+}).listen(7000)
+
+
+app.createServer((req, res)=> {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write("hello world");
+    res.end();
+}).listen(8000)
+
